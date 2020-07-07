75fb1d60e65e4519fcc0d2a2904346e63e313432
mark george <markgeorge@aucegypt.edu>
2020-07-07 17:02:37 +0200

    markversion

---
+++ headers/mark.h
@@ -0,0 +1,4 @@
+#ifndef MARK_H
+#define MARK_H
+void markadil2001();
+#endif
---
+++ sources/mark.cpp
@@ -0,0 +1,6 @@
+#include "mark.h"
+void markadil2001()
+{
+	cout<<"Mark George"<<endl;
+	cout<< "markgeorge@aucegypt.edu"<<endl;
+}
